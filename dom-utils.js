adba8aed27998b27e1815b183964b0008930ae16
SCREECH115 <arekw99@gmail.com>
2022-09-10 17:00:40 +0200

    Added dom-utils, displaying data in page

---
+++ dom-utils.js
@@ -0,0 +1,51 @@
+const createInfoElement = (labelName, value) => {
+  const infoElement = document.createElement("div");
+
+  const labelElement = document.createElement("strong");
+  labelElement.innerText = `${labelName}:`;
+  const valueElement = document.createElement("span");
+  valueElement.innerText = value;
+
+  infoElement.appendChild(labelElement);
+  infoElement.appendChild(valueElement);
+
+  return infoElement;
+};
+
+const createFlagImgElement = (country) => {
+  const imgElement = document.createElement("img");
+  imgElement.src = country.flagUrl;
+
+  return imgElement;
+};
+
+const createCountryItemElement = (country) => {
+  const countryElement = document.createElement("li");
+
+  const countryNameElement = document.createElement("span");
+  countryNameElement.innerText = country.name;
+
+  countryElement.appendChild(createFlagImgElement(country));
+  countryElement.appendChild(countryNameElement);
+
+  countryElement.appendChild(
+    createInfoElement("Population", country.population)
+  );
+  countryElement.appendChild(createInfoElement("Region", country.region));
+  countryElement.appendChild(createInfoElement("Capital", country.capital));
+
+  return countryElement;
+};
+
+const createListElement = (countries) => {
+  const listElement = document.createElement("ul");
+  countries.forEach((country) => {
+    listElement.appendChild(createCountryItemElement(country));
+  });
+  return listElement;
+};
+
+export const renderCountriesList = (countries) => {
+  const rootElement = document.querySelector("#root");
+  rootElement.appendChild(createListElement(countries));
+};
